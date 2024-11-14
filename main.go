3fb4540e8313e60dc37d42fbf2597a1ec174356d
fanhuageng <fanhuag0216@163.com>
2024-11-14 18:52:01 +0800

    项目启动

--- main.go
+++ main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-  "fmt"
+	"MyCache/httpServer"
+	single_cache "MyCache/single-cache"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
-func main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-  // to see how GoLand suggests fixing it.
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
-
-  for i := 1; i <= 5; i++ {
-	//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session, 
-	// right-click your code in the editor and select the <b>Debug</b> option. 
-	fmt.Println("i =", 100/i)
-  }
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func main() {
+	single_cache.NewGroup("scores", single_cache.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[slowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}), 2<<10)
+
+	addr := "localhost:9999"
+	peers := httpServer.NewHTTPPool(addr)
+	log.Println("MyCache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr, peers))
+}

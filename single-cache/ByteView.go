951eeb9389928a991b892e6895524109ab8b2cc7
fanhuageng <fanhuag0216@163.com>
2024-11-14 08:59:17 +0800

    single-cache

--- single-cache/ByteView.go
+++ single-cache/ByteView.go
@@ -1,1 +1,24 @@
 package single_cache
+
+// ByteView保存字节的不可变视图。
+type ByteView struct {
+	b []byte
+}
+
+func (bv ByteView) Len() int {
+	return len(bv.b)
+}
+
+func (bv ByteView) String() string {
+	return string(bv.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
+
+func (bv ByteView) ByteSlice() []byte {
+	return cloneBytes(bv.b)
+}
--- single-cache/mycache.go
+++ single-cache/mycache.go
@@ -1,1 +1,12 @@
 package single_cache
+
+type Getter interface {
+	Ger(key string) ([]byte, error)
+}
+
+type GetterFunc func(key string) ([]byte, error)
+
+// Get实现Getter接口函数
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}

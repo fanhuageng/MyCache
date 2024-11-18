a85000ce16ff2ac595e1910540d301efa28415db
fhg <1255960997@qq.com>
2024-11-18 15:34:45 +0800

    update: 添加多并发测试用例

--- run.sh
+++ run.sh
@@ -3,14 +3,56 @@ trap "rm server;kill 0" EXIT
 
 go build -o server
 #./server -port=8001 &
-./server -port=8002 -api=0 &
-./server -port=8003 -api=0 &
+#./server -port=8002 -api=0 &
+#./server -port=8003 -api=0 &
+./server -port=8001 &
+./server -port=8002 &
+./server -port=8003 -api=1 &
 
 sleep 2
-#echo ">>> start test"
-#curl "http://localhost:9999/api?key=a" &
-#curl "http://localhost:9999/api?key=a" &
-#curl "http://localhost:9999/api?key=a" &
+echo ">>> start test"
+
+
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+#curl "http://localhost:9999/api?key=d" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=a" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=g" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
+curl "http://localhost:9999/api?key=e" &
 
 wait
 
